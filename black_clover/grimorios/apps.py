3f5bdabd6bd216e89c2622fd0bb3d62c5afdf1ba
Amuravy <amuravy_5@hotmail.com>
2022-10-23 21:31:07 -0500

    add #noqa to skip flake8

--- black_clover/grimorios/apps.py
+++ black_clover/grimorios/apps.py
@@ -5,3 +5,9 @@ from django.utils.translation import gettext_lazy as _
 class GrimoriosConfig(AppConfig):
     name = "black_clover.grimorios"
     verbose_name = _("Grimorios")
+
+    def ready(self):
+        try:
+            import black_clover.grimorios.signals  # noqa.
+        except ImportError as e:
+            raise Exception(e)
--- black_clover/grimorios/urls.py
+++ black_clover/grimorios/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 
-from black_clover.grimorios.views import GetAllGrimorios, GetAllRegistration, Register
+from black_clover.grimorios.views import (
+    GetAllGrimorios,
+    GetAllRegistration,
+    Register,
+    UpdateStatus,
+)
 
 urlpatterns = [
     path("register/", Register.as_view(), name="register"),
     path("register/<uuid:uuid>/", Register.as_view(), name="register"),
     path("registration/", GetAllRegistration.as_view(), name="registration"),
     path("grimorios/", GetAllGrimorios.as_view(), name="grimorios"),
+    path("update_status/<uuid:uuid>/", UpdateStatus.as_view(), name="grimorios"),
 ]
